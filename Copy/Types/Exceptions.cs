2d8655ae872ba6f59618a2f4582141286b541721
FRFlo <62435410+FRFlo@users.noreply.github.com>
2024-03-11 10:01:38 +0100

    feat(Program.cs): Added core program

---
+++ Copy/Types/Exceptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Copy.Types
+{
+    /// <summary>
+    /// Exception thrown when a client is not found.
+    /// </summary>
+    /// <param name="message">The message to display.</param>
+    internal class ClientNotFoundException(string message) : Exception(message)
+    {
+    }
+}
