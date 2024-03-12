1274804f3c7cefee628878a69810a5912706508d
Flo <62435410+FRFlo@users.noreply.github.com>
2024-03-12 11:19:59 +0100

    feat(FTP.cs): Certificate validation

--- Copy/Clients/FTP.cs
+++ Copy/Clients/FTP.cs
@@ -30,6 +30,21 @@ namespace Copy.Clients
             _credentials = credentials;
             FtpClient = new FtpClient(credentials.Host, new NetworkCredential(credentials.Username, credentials.Password), credentials.Port);
             FtpClient.Config.EncryptionMode = FtpEncryptionMode.Auto;
+            FtpClient.ValidateCertificate += (client, args) =>
+            {
+                if (credentials.Fingerprint == null)
+                {
+                    args.Accept = true;
+                }
+                else if (args.Certificate.GetCertHashString() == credentials.Fingerprint)
+                {
+                    args.Accept = true;
+                }
+                else
+                {
+                    args.Accept = false;
+                }
+            };
             FtpClient.AutoConnect();
         }
 
