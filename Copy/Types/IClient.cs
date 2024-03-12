2a067bbb9318c28565ab088cba012b9f39265833
Flo <62435410+FRFlo@users.noreply.github.com>
2024-03-12 08:55:20 +0100

    docs(IClient.cs): Correction of path descriptions

--- Copy/Types/IClient.cs
+++ Copy/Types/IClient.cs
@@ -1,4 +1,4 @@
-﻿namespace Copy.Types
+namespace Copy.Types
 {
     /// <summary>
     /// Interface for a client used to copy, delete, and list files.
@@ -25,19 +25,19 @@
         /// <summary>
         /// Get a file.
         /// </summary>
-        /// <param name="path">Path to the directory.</param>
+        /// <param name="path">Path to the file.</param>
         /// <returns>Stream of the file.</returns>
         Stream GetFile(string path);
         /// <summary>
         /// Put a file.
         /// </summary>
-        /// <param name="path">Path to the directory.</param>
+        /// <param name="path">Path to the file.</param>
         /// <param name="stream">Stream of the file.</param>
         void PutFile(string path, Stream stream);
         /// <summary>
         /// Delete a file.
         /// </summary>
-        /// <param name="path">Path to the directory.</param>
+        /// <param name="path">Path to the file.</param>
         void DeleteFile(string path);
     }
 }
