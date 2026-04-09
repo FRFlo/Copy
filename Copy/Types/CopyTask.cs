372d1aeb1be6b483f9adac4747466b0d2b79abc8
Flo <62435410+FRFlo@users.noreply.github.com>
2026-04-09 09:44:21 +0200

    fix(Copy): allow optional task fields in generated config

--- Copy/Types/CopyTask.cs
+++ Copy/Types/CopyTask.cs
@@ -3,47 +3,52 @@
 namespace Copy.Types
 {
     /// <summary>
-    /// Task to copy files.
+    ///     Task to copy files.
     /// </summary>
     public class CopyTask
     {
         /// <summary>
-        /// Source of files to copy.
+        ///     Source of files to copy.
         /// </summary>
         [JsonProperty(PropertyName = "Source", Required = Required.Always)]
         public required CopyIO Source { get; set; }
+
         /// <summary>
-        /// Destination of files to copy.
+        ///     Destination of files to copy.
         /// </summary>
         [JsonProperty(PropertyName = "Destination", Required = Required.Always)]
         public required CopyIO Destination { get; set; }
+
         /// <summary>
-        /// Whether to delete files after copying.
+        ///     Whether to delete files after copying.
         /// </summary>
         [JsonProperty(PropertyName = "Delete", Required = Required.DisallowNull)]
-        public bool Delete { get; set; } = false;
+        public bool Delete { get; set; }
+
         /// <summary>
-        /// Optional location where the original file should be moved after a successful copy.
+        ///     Optional location where the original file should be moved after a successful copy.
         /// </summary>
-        [JsonProperty(PropertyName = "MoveOriginalTo", Required = Required.DisallowNull)]
-        public CopyIO? MoveOriginalTo { get; set; } = null;
+        [JsonProperty(PropertyName = "MoveOriginalTo", Required = Required.Default,
+            NullValueHandling = NullValueHandling.Ignore)]
+        public CopyIO? MoveOriginalTo { get; set; }
+
         /// <summary>
-        /// Filter for files to copy.
+        ///     Filter for files to copy.
         /// </summary>
         [JsonProperty(PropertyName = "Filter", Required = Required.DisallowNull)]
         public CopyFilter Filter { get; set; } = new();
 
         /// <summary>
-        /// Whether to zip the files when copying.
+        ///     Whether to zip the files when copying.
         /// </summary>
         [JsonProperty(PropertyName = "Zip", Required = Required.DisallowNull)]
-        public bool Zip { get; set; } = false;
+        public bool Zip { get; set; }
 
         /// <summary>
-        /// Custom name for the zip file. If not specified and Zip is true, 
-        /// a default name based on the timestamp will be used.
+        ///     Custom name for the zip file. If not specified and Zip is true,
+        ///     a default name based on the timestamp will be used.
         /// </summary>
-        [JsonProperty(PropertyName = "ZipFileName")]
+        [JsonProperty(PropertyName = "ZipFileName", NullValueHandling = NullValueHandling.Ignore)]
         public string? ZipFileName { get; set; }
     }
 }
